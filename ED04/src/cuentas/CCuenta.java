61d2089ae0d9fe3e35fbe6e2af27cd50ddc36f10
crist <crist@Cristina>
2024-03-04 22:40:10 +0100

    6. Insertar comentarios JavaDoc en la clase CCuenta

--- ED04/src/cuentas/CCuenta.java
+++ ED04/src/cuentas/CCuenta.java
@@ -1,4 +1,12 @@
 package cuentas;
+/**
+ * Representa una cuenta corriente asociada.
+ * 
+ * Incluye las operaciones de creación de la cuenta, consulta de estado, ingreso y retirada de efectivo.
+ * @author Cristina V. C.
+ * @version 1.0
+ * @since 04/03/2024
+ */
 public class CCuenta {
 
 
@@ -38,11 +46,22 @@ public class CCuenta {
 	private void setTipoInterés(double tipoInterés) {
 		this.tipoInterés = tipoInterés;
 	}
-
+	
+	/**
+	 * Constructor sin parámetros de la clase CCuenta
+	 */
 	public CCuenta()
     {
     }
 
+	/**
+	 * Construnctor de la cuenta corriente.
+	 * 
+	 * @param nom nombre del propietario de la cuenta
+	 * @param cue número de cuenta
+	 * @param sal saldo inicial
+	 * @param tipo tipo de interés
+	 */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         setNombre(nom);
@@ -50,11 +69,20 @@ public class CCuenta {
         setSaldo(sal);
     }
 
+    /**
+     * Obtiene el saldo actual de la cuenta.
+     * @return saldo actual.
+     */
     public double estado()
     {
         return getSaldo();
     }
 
+    /**
+     * Ingresa la cantidad especificada.
+     * @param cantidad a ingresar
+     * @throws Exception si la cantidad es negativa
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
@@ -62,6 +90,11 @@ public class CCuenta {
         setSaldo(getSaldo() + cantidad);
     }
 
+    /**
+     * Retira si es posible la cantidad indicada de la cuenta
+     * @param cantidad
+     * @throws Exception si la cantidad es negativa o mayor al saldo de la cuenta.
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
